6cc316f1bda68b42e39984d4531b2f6d60b2331c
Anton Romanovich <anthony.romanovich@gmail.com>
2025-06-25 00:10:08 +0200

    random progress

--- apps/api/sharedLibs/go-html-to-md/html-to-markdown.go
+++ apps/api/sharedLibs/go-html-to-md/html-to-markdown.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"C"
-	"fmt"
 	"log"
 
-	"github.com/JohannesKaufmann/html-to-markdown/v2/converter"
+	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
 )
 
 //export ConvertHTMLToMarkdown
 func ConvertHTMLToMarkdown(html *C.char) *C.char {
-	converter := converter.NewConverter()
-
-	markdown, err := converter.ConvertString(C.GoString(html))
+	markdown, err := htmltomarkdown.ConvertString(C.GoString(html))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Converted HTML to Markdown:\n", markdown)
 	return C.CString(markdown)
 }
 
